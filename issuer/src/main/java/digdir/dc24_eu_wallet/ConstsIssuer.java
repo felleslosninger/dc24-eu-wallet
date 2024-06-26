49fa54039ab910174820fa5112a7d354809581cb
Langbakk <solveig.langbakk@digdir.no>
2024-06-26 13:23:45 +0200

    refactor: add getter of enums

--- issuer/src/main/java/digdir/dc24_eu_wallet/ConstsIssuer.java
+++ issuer/src/main/java/digdir/dc24_eu_wallet/ConstsIssuer.java
@@ -3,4 +3,16 @@ enum ConstsIssuer {
     VERSION = "1",
     HEALTH = "health",
     ISSUER = "issuer"
+
+    private final String value;
+
+    ConstsIssuer(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
+
+
