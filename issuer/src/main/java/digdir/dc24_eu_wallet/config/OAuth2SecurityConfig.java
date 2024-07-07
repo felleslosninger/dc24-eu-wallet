0380975c53ad51696dce9fc1567144543c93c5cf
Solveig <solvlang@stud.ntnu.no>
2024-07-07 21:53:50 +0200

    create login with ansattporten

---
+++ issuer/src/main/java/digdir/dc24_eu_wallet/config/OAuth2SecurityConfig.java
@@ -0,0 +1,67 @@
+package digdir.dc24_eu_wallet.config;
+
+import java.util.function.Consumer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+
+@Configuration
+@EnableWebFluxSecurity
+public class OAuth2SecurityConfig {
+
+  @Autowired
+  private ReactiveClientRegistrationRepository clientRegistrationRepository;
+
+  @Bean
+  public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    http
+      .authorizeExchange(exchanges ->
+        exchanges
+          .pathMatchers("/", "/error", "/logout/callback").permitAll()
+          .anyExchange().authenticated()
+      )
+      .oauth2Login(oauth2 -> oauth2
+        .authorizationRequestResolver(authorizationRequestResolver(this.clientRegistrationRepository)))
+      .oidcLogout(logout -> logout
+        .backChannel(Customizer.withDefaults()))
+      .logout(logout -> logout
+        .logoutSuccessHandler(oidcLogoutSuccessHandler()));
+    return http.build();
+  }
+
+  private ServerOAuth2AuthorizationRequestResolver authorizationRequestResolver(
+    ReactiveClientRegistrationRepository clientRegistrationRepository) {
+
+    DefaultServerOAuth2AuthorizationRequestResolver authorizationRequestResolver = new DefaultServerOAuth2AuthorizationRequestResolver(clientRegistrationRepository);
+    authorizationRequestResolver.setAuthorizationRequestCustomizer(authorizationRequestCustomizer());
+    return  authorizationRequestResolver;
+  }
+
+  private Consumer<OAuth2AuthorizationRequest.Builder> authorizationRequestCustomizer() {
+    return customizer -> customizer
+      .additionalParameters(params ->
+        params.put( "authorization_details",
+          "[{\"type\":\"ansattporten:altinn:service\",\"resource\": \"urn:altinn:resource:2480:40\", \"allow_multiple_organizations\": \"true\"}]"));
+  }
+
+  private ServerLogoutSuccessHandler oidcLogoutSuccessHandler() {
+    OidcClientInitiatedServerLogoutSuccessHandler oidcLogoutSuccessHandler =
+      new OidcClientInitiatedServerLogoutSuccessHandler(this.clientRegistrationRepository);
+
+    // Sets the location that the End-User's User Agent will be redirected to
+    // after the logout has been performed at the Provider
+    oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/logout/callback");
+
+    return oidcLogoutSuccessHandler;
+  }
+}
---
+++ issuer/src/main/java/digdir/dc24_eu_wallet/web/AnsattportenIntegrationController.java
@@ -0,0 +1,31 @@
+package digdir.dc24_eu_wallet.web;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AnsattportenIntegrationController {
+  @GetMapping("/")
+  public String index(){
+    return "index";
+  }
+
+  @GetMapping("/ansattporten_authentication")
+  public String user(Model model,
+                     @AuthenticationPrincipal OidcUser oidcUser) {
+    model.addAttribute("idtoken", oidcUser.getIdToken().getTokenValue());
+    model.addAttribute("pid", oidcUser.getUserInfo().getClaim("pid"));
+    model.addAttribute("authorizationdetails", oidcUser.getUserInfo().getClaim("authorization_details"));
+    model.addAttribute("name", oidcUser.getFullName());
+    return "ansattporten-authenticated";
+  }
+
+  @GetMapping("/logout/callback")
+  public String logoutCallback() {
+    return "logout";
+  }
+}
+
