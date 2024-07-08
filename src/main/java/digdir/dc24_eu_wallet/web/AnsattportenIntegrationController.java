41df31eadc78aa4e20e65928fce83538e0e9fb41
Langbakk <solveig.langbakk@digdir.no>
2024-07-08 15:07:20 +0200

    implement getter and setter

--- src/main/java/digdir/dc24_eu_wallet/web/AnsattportenIntegrationController.java
+++ src/main/java/digdir/dc24_eu_wallet/web/AnsattportenIntegrationController.java
@@ -1,13 +1,19 @@
 package digdir.dc24_eu_wallet.web;
 
+import net.minidev.json.JSONObject;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Collection;
+
 @Controller
 public class AnsattportenIntegrationController {
+
+  private Collection claims;
+
   @GetMapping("/")
   public String index(){
     return "index";
@@ -20,6 +26,7 @@ public class AnsattportenIntegrationController {
     model.addAttribute("pid", oidcUser.getUserInfo().getClaim("pid"));
     model.addAttribute("authorizationdetails", oidcUser.getUserInfo().getClaim("authorization_details"));
     model.addAttribute("name", oidcUser.getFullName());
+    setClaims(oidcUser);
     return "ansattporten-authenticated";
   }
 
@@ -27,5 +34,15 @@ public class AnsattportenIntegrationController {
   public String logoutCallback() {
     return "logout";
   }
-}
 
+  public void setClaims(OidcUser user){
+    this.claims = claims;
+  }
+
+  public Collection getClaims(){
+    return claims;
+  }
+
+
+
+}
