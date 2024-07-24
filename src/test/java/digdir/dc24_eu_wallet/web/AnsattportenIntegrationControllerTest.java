49ff56775c42d6a8612e8a6c55be216ee3a25cb4
Magnus Løwer <magnus.lower@gmail.com>
2024-07-24 09:33:07 +0200

    Created a simple test class for src/main/java/digdir.dc24_eu_wallet/web/AnsattportenIntegrationController. Not yet done (three methods needs to be implemented)

---
+++ src/test/java/digdir/dc24_eu_wallet/web/AnsattportenIntegrationControllerTest.java
@@ -0,0 +1,34 @@
+package digdir.dc24_eu_wallet.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnsattportenIntegrationControllerTest {
+
+    private AnsattportenIntegrationController ansattportenIntegrationController;
+
+    @BeforeEach
+    void setUp() {
+        ansattportenIntegrationController = new AnsattportenIntegrationController();
+    }
+
+    @AfterEach
+    void tearDown() {
+        ansattportenIntegrationController = null;
+    }
+
+    @Test
+    void index() {
+    }
+
+    @Test
+    void user() {
+    }
+
+    @Test
+    void logoutCallback() {
+    }
+}
