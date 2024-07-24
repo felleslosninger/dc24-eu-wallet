0a98845b45dda3969e35791d3084cc7a5939287b
Magnus Løwer <magnus.lower@gmail.com>
2024-07-24 09:31:32 +0200

    Created a simple test class for src/main/java/digdir.dc24_eu_wallet/service/RequestService. Not yet done (one method needs to be implemented)

---
+++ src/test/java/digdir/dc24_eu_wallet/service/RequestServiceTest.java
@@ -0,0 +1,31 @@
+package digdir.dc24_eu_wallet.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestServiceTest {
+
+    private RequestService requestService;
+
+    @BeforeEach
+    void setUp() {
+        String audienceUrl = "http://example.com/audience";
+        String mattrUrl = "http://example.com/mattr";
+        String clientSecret = "dummySecret";
+        String clientId = "dummyClientId";
+
+        requestService = new RequestService(audienceUrl, mattrUrl, clientSecret, clientId);
+    }
+
+    @AfterEach
+    void tearDown() {
+        requestService = null;
+    }
+
+    @Test
+    void getJwt() {
+    }
+}
