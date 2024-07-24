528918bf7dbbbda0358628120b7a4171ac43f4ae
Magnus Løwer <magnus.lower@gmail.com>
2024-07-24 08:10:13 +0200

    Created a simple test class for src/main/java/digdir.dc24_eu_wallet/dto/CredentialDTO. Not yet done (four methods needs to be implemented)

---
+++ src/test/java/digdir/dc24_eu_wallet/dto/CredentialDTOTest.java
@@ -0,0 +1,41 @@
+package digdir.dc24_eu_wallet.dto;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CredentialDTOTest {
+
+    private CredentialDTO credentialDTO;
+
+    @BeforeEach
+    void setUp() {
+        credentialDTO = new CredentialDTO();
+    }
+
+    @AfterEach
+    void tearDown() {
+        credentialDTO = null;
+    }
+
+    @Test
+    void getCred() {
+    }
+
+    @Test
+    void addCred() {
+    }
+
+    @Test
+    void setCred() {
+    }
+
+    @Test
+    void isValid() {
+    }
+}
