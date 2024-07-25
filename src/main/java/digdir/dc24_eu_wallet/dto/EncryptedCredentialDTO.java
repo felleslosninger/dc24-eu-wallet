1a437e8d7d6533e5978be54847ae0f1f66804362
Magnus Løwer <magnus.lower@gmail.com>
2024-07-25 11:37:49 +0200

    Formatted src/main/java/digdir.dc24_eu_wallet/dto/EncryptedCredentialDTO according to suggestions from SonarLint

--- src/main/java/digdir/dc24_eu_wallet/dto/EncryptedCredentialDTO.java
+++ src/main/java/digdir/dc24_eu_wallet/dto/EncryptedCredentialDTO.java
@@ -2,6 +2,9 @@ package digdir.dc24_eu_wallet.dto;
 
 import com.google.gson.annotations.SerializedName;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
 /**
@@ -10,117 +13,59 @@ import java.util.List;
  * @author Daniel Neset
  * @version 16.07.2024
  */
+@Getter
 public class EncryptedCredentialDTO {
   private Jwe jwe;
 
-  public Jwe getJwe() {
-    return jwe;
-  }
-
-  /**
+    /**
    * Represents th JSON Web Encryption (JWE) structure.
    */
+  @Getter
   public static class Jwe {
+
+    // Getters
     @SerializedName("protected")
     private String protectedInfo;
     private List<Recipient> recipients;
     private String ciphertext;
     private String iv;
     private String tag;
-
-    // Getters
-    public String getProtectedInfo() {
-      return protectedInfo;
-    }
-
-    public List<Recipient> getRecipients() {
-      return recipients;
-    }
-
-    public String getCiphertext() {
-      return ciphertext;
-    }
-
-    public String getIv() {
-      return iv;
-    }
-
-    public String getTag() {
-      return tag;
-    }
   }
 
   /**
    * Represents a recipient of the JSON Web Encryption (JWE).
    */
+  @Setter
+  @Getter
   public static class Recipient {
-    private Header header;
-    private String encrypted_key;
 
     // Getters
-    public Header getHeader() {
-      return header;
-    }
-
-    public String getEncryptedKey() {
-      return encrypted_key;
-    }
-
-    public void setHeader(Header header) {
-      this.header = header;
-    }
-
-    public void setEncryptedKey(String encryptedKey) {
-      this.encrypted_key = encryptedKey;
-    }
+    private Header header;
+    private String encryptedKey;
   }
 
   /**
    * Represents the header of a recipient in the JSON Web Encryption (JWE).
    */
+  @Getter
   public static class Header {
+
+    // Getters
     private String alg;
     private String kid;
     private String skid;
     private Epk epk;
-
-    // Getters
-    public String getAlg() {
-      return alg;
-    }
-
-    public String getKid() {
-      return kid;
-    }
-
-    public String getSkid() {
-      return skid;
-    }
-
-    public Epk getEpk() {
-      return epk;
-    }
   }
 
   /**
    * Represents the Ephemeral Public Key (EPK) used in the encryption process within the JWE header.
    */
+  @Getter
   public static class Epk {
+
+    // Getters
     private String kty;
     private String crv;
     private String x;
-
-    // Getters
-    public String getKty() {
-      return kty;
-    }
-
-    public String getCrv() {
-      return crv;
-    }
-
-    public String getX() {
-      return x;
-    }
   }
 }
