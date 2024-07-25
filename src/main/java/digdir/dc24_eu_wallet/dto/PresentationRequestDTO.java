241d1a1b5caac9d2d557bdd20ff08f5c1537ffc0
Magnus Løwer <magnus.lower@gmail.com>
2024-07-25 11:43:20 +0200

    Formatted src/main/java/digdir.dc24_eu_wallet/dto/PresentationRequestDTO according to suggestions from SonarLint

--- src/main/java/digdir/dc24_eu_wallet/dto/PresentationRequestDTO.java
+++ src/main/java/digdir/dc24_eu_wallet/dto/PresentationRequestDTO.java
@@ -2,15 +2,19 @@ package digdir.dc24_eu_wallet.dto;
 
 import com.google.gson.annotations.Expose;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
- * This class contains the data witch is sent
+ * This class contains the data which is sent
  * to Mattr to create the QR code.
  *
  * @author Daniel Neset
  * @version 16.07.2024
  */
+@Setter
+@Getter
 public class PresentationRequestDTO {
-
   @Expose
   private String challenge;
   @Expose
@@ -19,36 +23,4 @@ public class PresentationRequestDTO {
   private String templateId;
   @Expose
   private String callbackUrl;
-
-  public String getChallenge() {
-    return challenge;
-  }
-
-  public void setChallenge(String challenge) {
-    this.challenge = challenge;
-  }
-
-  public String getDid() {
-    return did;
-  }
-
-  public void setDid(String did) {
-    this.did = did;
-  }
-
-  public String getTemplateId() {
-    return templateId;
-  }
-
-  public void setTemplateId(String templateId) {
-    this.templateId = templateId;
-  }
-
-  public String getCallbackUrl() {
-    return callbackUrl;
-  }
-
-  public void setCallbackUrl(String callbackUrl) {
-    this.callbackUrl = callbackUrl;
-  }
 }
