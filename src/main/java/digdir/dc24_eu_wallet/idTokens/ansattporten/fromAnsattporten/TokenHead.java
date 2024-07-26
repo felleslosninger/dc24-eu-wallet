3e3edcd16fd8f59a4ed78220f75078b45740f7b8
Magnus Løwer <magnus.lower@gmail.com>
2024-07-26 09:16:14 +0200

    Commented on src/main/java/digdir.dc24_eu_wallet/idTokens/ansattporten/fromAnsattporten/TokenHead for clarity

--- src/main/java/digdir/dc24_eu_wallet/idTokens/ansattporten/fromAnsattporten/TokenHead.java
+++ src/main/java/digdir/dc24_eu_wallet/idTokens/ansattporten/fromAnsattporten/TokenHead.java
@@ -33,21 +33,35 @@ public class TokenHead {
   /**
    * Constructs the token payload object. Initializes lists for parts that
    * contain several elements.
-   *
    */
   public TokenHead() {
         amr = new ArrayList<>();
         authorization_details = new ArrayList<>();
     }
 
+    /**
+     * Gets the list of AMR.
+     *
+     * @return List om AMR values
+     */
     public List<String> getAmr() {
     return amr;
   }
 
+    /**
+     * Gets the list of authorization details.
+     *
+     * @return List of AuthorizationDetails objects
+     */
   public List<AutorizationDetails> getAuthorizationDetails() {
     return authorization_details;
   }
 
+    /**
+     * Sets the list of authorization details.
+     *
+     * @param authorizationDetails List of AuthorizationDetails objects to set
+     */
   public void setAuthorizationDetails(List<AutorizationDetails> authorizationDetails) {
     this.authorization_details = authorizationDetails;
   }
